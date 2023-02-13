f8b30e80a807f6b247d965cb85adb07a557395b8
Anatoliy Matsakov <mactolik1993@gmail.com>
2023-02-14 00:35:13 +0200

    Task 10 done

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,29 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const input = document.querySelector('input');
+const btnCreate = document.querySelector('[data-create]');
+const btnDestroy = document.querySelector('[data-destroy]');
+const divEl = document.querySelector('#boxes');
+
+btnCreate.addEventListener('click', createBoxes);
+
+btnDestroy.addEventListener('click', destroyBoxes);
+
+function destroyBoxes() {
+  divEl.innerHTML = '<div id="boxes"></div>';
+}
+
+function createBoxes(amount) {
+  amount = input.value;
+
+  for (let index = 0; index < amount; index += 1) {
+    let div = document.createElement('div');
+    div.style.width = index * 10 + 30 + 'px';
+    div.style.height = index * 10 + 30 + 'px';
+    div.style.backgroundColor = getRandomHexColor();
+    console.log(div);
+    divEl.append(div);
+  }
+}
