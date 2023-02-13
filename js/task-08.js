33fd87c505162412337a5ba419f195e4d3b0cc18
Anatoliy Matsakov <mactolik1993@gmail.com>
2023-02-13 21:23:56 +0200

    Task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+
+  if (email === '' || password === '') {
+    alert('All data must be filled');
+    return;
+  }
+
+  const formData = {
+    email,
+    password,
+  };
+
+  console.log(formData);
+  formEl.reset();
+}
