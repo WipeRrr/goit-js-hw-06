39410d6317b7b750b66f4d93b54c28b1619dcf46
Anatoliy Matsakov <mactolik1993@gmail.com>
2023-02-13 21:36:15 +0200

    Task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnColor = document.querySelector('.change-color');
+const changedColor = document.querySelector('.color');
+const bodyEl = document.querySelector('body');
+console.log(bodyEl.style)
+btnColor.addEventListener('click', onBtnColorClick);
+
+function onBtnColorClick() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  changedColor.textContent = getRandomHexColor();
+}
