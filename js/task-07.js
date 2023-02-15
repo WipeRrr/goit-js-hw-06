443d76f104083154916aece6020a80c38ebc4eae
Anatoliy Matsakov <mactolik1993@gmail.com>
2023-02-15 12:15:40 +0200

    7 task bug fix

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,9 @@
 const sizeControlBtn = document.querySelector('#font-size-control');
 const labelText = document.querySelector('#text')
-console.log(labelText.fontSize);
+
+
 sizeControlBtn.addEventListener('input', onBtnScroll);
+labelText.style.fontSize = sizeControlBtn.value+'px';
 
 function onBtnScroll(event) {
     
