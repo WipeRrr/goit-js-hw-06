1cfbc872835445f89a30713774baf25db16a3fa8
Anatoliy Matsakov <mactolik1993@gmail.com>
2023-02-15 12:26:55 +0200

    6 task bug fix

--- js/task-06.js
+++ js/task-06.js
@@ -3,7 +3,7 @@ const input = document.querySelector('#validation-input');
 input.addEventListener('blur', onInputChange);
 
 function onInputChange(event) {
-  if (event.currentTarget.value.length <= input.dataset.length) {
+  if (event.currentTarget.value.length == Number(input.dataset.length)) {
     input.classList.add('valid');
     input.classList.remove('invalid');
   } else input.classList.add('invalid');
